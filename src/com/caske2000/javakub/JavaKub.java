501c5022a5edb94274874fc78852b2fa98c89ea8
Caske2000 <vdlucas18@gmail.com>
2015-08-03 22:28:47 +0200

    Created a basic rendering loop
    Thanks to ScratchForFun

---
+++ src/com/caske2000/javakub/JavaKub.java
@@ -0,0 +1,18 @@
+package com.caske2000.javakub;
+
+/**
+ * Created by Caske2000 on 03/08/2015.
+ */
+public class JavaKub
+{
+    public static void main(String[] args)
+    {
+        new JavaKub();
+    }
+
+    public JavaKub()
+    {
+        Window window = new Window("JavaKub-preAlpha", 1280, 720);
+        window.start();
+    }
+}
